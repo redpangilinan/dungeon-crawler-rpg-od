d15546e4a931376b07b93cfcbe1f28d0d51dcc3c
redpangilinan <redpangilinan715@gmail.com>
2022-12-21 18:31:45 +0800

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const showPlayer = document.querySelector("#player");
+
+showPlayer.innerHTML(player.name);
+
+const player = {
+    name: "Red",
+    health: 100,
+    attack: 25,
+    defense: 20
+}
