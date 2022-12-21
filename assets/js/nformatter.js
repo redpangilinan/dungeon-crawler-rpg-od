0f71f9ea84665ae6fcec18ee31cf8714a8822f39
redpangilinan <redpangilinan715@gmail.com>
2022-12-22 04:55:46 +0800

    Implemented formatting for numbers if values get too high

---
+++ assets/js/nformatter.js
@@ -0,0 +1,16 @@
+function nFormatter(num) {
+    const lookup = [
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "k" },
+        { value: 1e6, symbol: "M" },
+        { value: 1e9, symbol: "G" },
+        { value: 1e12, symbol: "T" },
+        { value: 1e15, symbol: "P" },
+        { value: 1e18, symbol: "E" }
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    var item = lookup.slice().reverse().find(function (item) {
+        return num >= item.value;
+    });
+    return item ? (num / item.value).toFixed(2).replace(rx, "$1") + item.symbol : "0";
+}
