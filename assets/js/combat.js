b533850add89ab31f895078087ac31c90af943fb
redpangilinan <redpangilinan715@gmail.com>
2022-12-23 06:33:50 +0800

    Basic damage calculation

--- assets/js/combat.js
+++ assets/js/combat.js
@@ -21,12 +21,23 @@ const hpValidation = () => {
 // ========== Combat ==========
 const playerAttack = () => {
     // Calculates the damage and attacks the enemy
-    let playerDamage = player.advStats.atk*(player.advStats.atk/(player.advStats.atk+enemy.advStats.def));
-    enemy.baseStats.hp -= playerDamage();
+    let damage = player.advStats.atk * (player.advStats.atk / (player.advStats.atk + enemy.advStats.def));
+    // Randomizes the damage by 90% - 110%
+    let dmgRange = 0.9 + Math.random() * 0.2;
+    damage = Math.round(damage * dmgRange);
+
+    enemy.baseStats.hp -= damage;
+    console.log(damage);
 }
 
 const enemyAttack = () => {
     // Calculates the damage and attacks the player
-    let enemyDamage = enemy.advStats.atk*(enemy.advStats.atk/(enemy.advStats.atk+player.advStats.def));
-    player.baseStats.hp -= enemyDamage();
+    let damage = enemy.advStats.atk * (enemy.advStats.atk / (enemy.advStats.atk + player.advStats.def));
+    // Randomizes the damage by 90% - 110%
+    let dmgRange = 0.9 + Math.random() * 0.2;
+    damage = Math.round(damage * dmgRange);
+
+    player.baseStats.hp -= damage;
+    console.log(damage);
+    playerLoadStats();
 }
