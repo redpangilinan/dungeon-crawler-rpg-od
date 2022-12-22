e23278680fe8aaaaa32336f913e19267cab21648
redpangilinan <redpangilinan715@gmail.com>
2022-12-23 06:33:55 +0800

    Create enemy.js

---
+++ assets/js/enemy.js
@@ -0,0 +1,27 @@
+// Enemy
+const enemy = {
+    name: 'Enemy',
+    lvl: 1,
+    baseStats: {
+        hp: 100,
+        hpMax: 100,
+        mp: 100,
+        mpMax: 100,
+        str: 10,
+        dex: 10,
+        vit: 10,
+        int: 10
+    },
+    advStats: {
+        atk: 0,
+        mAtk: 0,
+        def: 0,
+        mDef: 0,
+        hit: 0,
+        eva: 0,
+        atkSpd: 1,
+        lifesteal: 0,
+        critRate: 0,
+        critDmg: 0,
+    },
+};
