37108404f016649d31920c1902626aab6eb60284
redpangilinan <redpangilinan715@gmail.com>
2022-12-23 21:27:59 +0800

    Implemented a combat backlog

--- assets/js/combat.js
+++ assets/js/combat.js
@@ -1,11 +1,11 @@
 // ========== Validation ==========
 const playerSlain = () => {
     // Soon
-}
+};
 
 const enemySlain = () => {
     // Soon
-}
+};
 
 const hpValidation = () => {
     // Prioritizes player death before the enemy
@@ -16,7 +16,7 @@ const hpValidation = () => {
             enemySlain();
         }
     }
-}
+};
 
 // ========== Combat ==========
 const playerAttack = () => {
@@ -25,10 +25,11 @@ const playerAttack = () => {
     // Randomizes the damage by 90% - 110%
     let dmgRange = 0.9 + Math.random() * 0.2;
     damage = Math.round(damage * dmgRange);
+    action = "Basic Attack";
 
     enemy.baseStats.hp -= damage;
-    console.log(damage);
-}
+    addCombatLog(`${player.name} used ${action} on ${enemy.name} and dealt ${damage} damage.`);
+};
 
 const enemyAttack = () => {
     // Calculates the damage and attacks the player
@@ -36,8 +37,35 @@ const enemyAttack = () => {
     // Randomizes the damage by 90% - 110%
     let dmgRange = 0.9 + Math.random() * 0.2;
     damage = Math.round(damage * dmgRange);
+    action = "Basic Attack";
 
     player.baseStats.hp -= damage;
-    console.log(damage);
+    addCombatLog(`${enemy.name} used ${action} on ${player.name} and dealt ${damage} damage.`);
     playerLoadStats();
-}
+};
+
+// ========== Combat Backlog ==========
+const combatBacklog = [];
+
+// Add a message to the backlog and update the display
+const addCombatLog = (message) => {
+    combatBacklog.push(message);
+    updateCombatLog();
+};
+
+// Update the message container element with the current messages in the backlog
+const updateCombatLog = () => {
+    // Clear the message container
+    let combatLogBox = document.getElementById("combatLogBox");
+    combatLogBox.innerHTML = "";
+
+    // Add the messages to the container
+    for (let message of combatBacklog) {
+        let logElement = document.createElement("p");
+        logElement.textContent = message;
+        combatLogBox.appendChild(logElement);
+    }
+
+    // Scroll to the bottom of the container
+    combatLogBox.scrollTop = combatLogBox.scrollHeight;
+};
