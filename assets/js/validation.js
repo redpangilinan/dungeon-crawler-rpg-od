edf55bd693f4e5b65d2fd1cf9569ff8f91e58f7a
redpangilinan <redpangilinan715@gmail.com>
2022-12-30 18:38:08 +0800

    Added a loading screen

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,7 +1,18 @@
-window.addEventListener("click", function () {
+document.querySelector("#title-screen").addEventListener("click", function () {
     if (player.savedata == null) {
-        window.location.href = '/pages/character_creation.html';
+        this.style.display = "none";
+        runLoad("character-creation", "flex");
     } else {
-        window.location.href = '/pages/hub.html';
+        this.style.display = "none";
+        runLoad("hub", "flex");
     }
 });
+
+const runLoad = (id, display) => {
+    let loader = document.querySelector("#loading");
+    loader.style.display = "flex";
+    setTimeout(async () => {
+        loader.style.display = "none";
+        document.querySelector(`#${id}`).style.display = `${display}`;
+    }, 1000);
+}
