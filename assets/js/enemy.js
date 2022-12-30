9461646b8c1f45a5bb1d1fb5c06c412aa51c25f5
redpangilinan <redpangilinan715@gmail.com>
2022-12-30 18:38:28 +0800

    Reworked stats

--- assets/js/enemy.js
+++ assets/js/enemy.js
@@ -2,26 +2,14 @@
 const enemy = {
     name: 'Enemy',
     lvl: 1,
-    baseStats: {
-        hp: 400,
-        hpMax: 100,
-        mp: 100,
-        mpMax: 100,
-        str: 200,
-        dex: 200,
-        vit: 300,
-        int: 10
-    },
-    advStats: {
+    stats: {
+        hp: null,
+        hpMax: null,
         atk: 0,
-        mAtk: 0,
         def: 0,
-        mDef: 0,
-        atkSpd: 1,
-        castTimeReduction: 0,
+        atkSpd: 0,
         lifesteal: 0,
-        mLifesteal: 0,
         critRate: 0,
-        critDmg: 0,
-    },
+        critDmg: 0
+    }
 };
