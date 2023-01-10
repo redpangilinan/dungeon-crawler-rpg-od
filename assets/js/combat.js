09e86cca50f5e7a7ec7f5c74253af020ac918011
redpangilinan <redpangilinan715@gmail.com>
2023-01-10 19:22:03 +0800

    Refractored names and added lifesteal functionality

--- assets/js/combat.js
+++ assets/js/combat.js
@@ -1,12 +1,14 @@
+const combatPanel = document.querySelector("#combatPanel")
+
 // ========== Validation ==========
 const hpValidation = () => {
     // Prioritizes player death before the enemy
-    if (player.baseStats.hp < 1) {
-        player.baseStats.hp = 0;
+    if (player.stats.hp < 1) {
+        player.stats.hp = 0;
         addCombatLog(`${player.name} died!`);
         endCombat();
-    } else if (enemy.baseStats.hp < 1) {
-        enemy.baseStats.hp = 0;
+    } else if (enemy.stats.hp < 1) {
+        enemy.stats.hp = 0;
         addCombatLog(`${enemy.name} died!`);
         endCombat();
     }
@@ -15,20 +17,22 @@ const hpValidation = () => {
 // ========== Attack Functions ==========
 const playerAttack = () => {
     // Calculates the damage and attacks the enemy
-    let damage = player.advStats.atk * (player.advStats.atk / (player.advStats.atk + enemy.advStats.def));
+    let damage = player.stats.atk * (player.stats.atk / (player.stats.atk + enemy.stats.def));
+    let lifesteal = player.stats.atk * (player.stats.vamp/100);
     // Randomizes the damage by 90% - 110%
     let dmgRange = 0.9 + Math.random() * 0.2;
     damage = damage * dmgRange;
     // Check if the attack is a critical hit
-    if (Math.floor(Math.random() * 101) <= player.advStats.critRate) {
+    if (Math.floor(Math.random() * 101) <= player.stats.critRate) {
         dmgtype = "crit damage";
-        damage = Math.round(damage * (1+(player.advStats.critDmg/100)));
+        damage = Math.round(damage * (1+(player.stats.critDmg/100)));
     } else {
         dmgtype = "damage";
         damage = Math.round(damage);
     }
 
-    enemy.baseStats.hp -= damage;
+    enemy.stats.hp -= damage;
+    player.stats.hp += lifesteal;
     addCombatLog(`${player.name} dealt ` + nFormatter(damage) + ` ${dmgtype} to ${enemy.name}.`);
     hpValidation();
     enemyLoadStats();
@@ -36,20 +40,22 @@ const playerAttack = () => {
 
 const enemyAttack = () => {
     // Calculates the damage and attacks the player
-    let damage = enemy.advStats.atk * (enemy.advStats.atk / (enemy.advStats.atk + player.advStats.def));
+    let damage = enemy.stats.atk * (enemy.stats.atk / (enemy.stats.atk + player.stats.def));
+    let lifesteal = enemy.stats.atk * (enemy.stats.vamp/100);
     // Randomizes the damage by 90% - 110%
     let dmgRange = 0.9 + Math.random() * 0.2;
     damage = damage * dmgRange;
     // Check if the attack is a critical hit
-    if (Math.floor(Math.random() * 101) <= enemy.advStats.critRate) {
+    if (Math.floor(Math.random() * 101) <= enemy.stats.critRate) {
         dmgtype = "crit damage";
-        damage = Math.round(damage * (1+(enemy.advStats.critDmg/100)));
+        damage = Math.round(damage * (1+(enemy.stats.critDmg/100)));
     } else {
         dmgtype = "damage";
         damage = Math.round(damage);
     }
 
-    player.baseStats.hp -= damage;
+    player.stats.hp -= damage;
+    enemy.stats.hp += lifesteal;
     addCombatLog(`${enemy.name} dealt ` + nFormatter(damage) + ` ${dmgtype} to ${player.name}.`);
     hpValidation();
     playerLoadStats();
@@ -81,13 +87,15 @@ const updateCombatLog = () => {
 // Combat Timer
 let combatSeconds = 0;
 const startCombat = () => {
+    battlePanel.style.display = "flex";
     addCombatLog(`${player.name} encountered ${enemy.name}.`);
-    playerTimer = setInterval(playerAttack, (1000 / player.advStats.atkSpd));
-    enemyTimer = setInterval(enemyAttack, (1000 / enemy.advStats.atkSpd));
+    playerTimer = setInterval(playerAttack, (1000 / player.stats.atkSpd));
+    enemyTimer = setInterval(enemyAttack, (1000 / enemy.stats.atkSpd));
     combatTimer = setInterval(combatCounter, 1000);
 };
 
 const endCombat = () => {
+    battlePanel.style.display = "flex";
     clearInterval(playerTimer);
     clearInterval(enemyTimer);
     clearInterval(combatTimer);
