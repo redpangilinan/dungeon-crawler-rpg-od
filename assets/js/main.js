456a23acb548b6c323c6f2638ebd1d83a509b774
redpangilinan <redpangilinan715@gmail.com>
2023-01-11 20:11:54 +0800

    Removed unnecessary code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const playerNameElement = document.getElementById('player-name');
 const playerLvlElement = document.getElementById('player-lvl');
 const playerHpElement = document.getElementById('player-hp');
+const playerCombatHpElement = document.getElementById('player-hp-battle');
 const playerExpElement = document.getElementById('player-exp');
 const playerAtkElement = document.getElementById('player-atk');
 const playerDefElement = document.getElementById('player-def');
@@ -9,19 +10,9 @@ const playerVampElement = document.getElementById('player-vamp');
 const playerCrateElement = document.getElementById('player-crate');
 const playerCdmgElement = document.getElementById('player-cdmg');
 
-const enemyHpElement = document.getElementById('enemy-hp');
-
 const exp = document.querySelector('#exp');
 
 // Gain exp
 // exp.addEventListener("click", function () {
 //     playerExpGain();
 // });
-
-// ========== Const Functions =========
-const enemyLoadStats = () => {
-    // Shows proper percentage for respective stats
-    let enemyHpPercentage = ((enemy.stats.hp / enemy.stats.hpMax) * 100).toFixed(2);
-
-    enemyHpElement.innerHTML = nFormatter(enemy.stats.hp) + "/" + nFormatter(enemy.stats.hpMax) + " (" + enemyHpPercentage + "%)";
-};
