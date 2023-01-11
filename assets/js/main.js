4f323a7bcc8fc4bf4f32e7117144932ca7d7e100
redpangilinan <redpangilinan715@gmail.com>
2023-01-11 22:02:15 +0800

    Removed unnecessary code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,9 @@ const playerNameElement = document.getElementById('player-name');
 const playerLvlElement = document.getElementById('player-lvl');
 const playerHpElement = document.getElementById('player-hp');
 const playerCombatHpElement = document.getElementById('player-hp-battle');
-const playerExpElement = document.getElementById('player-exp');
 const playerAtkElement = document.getElementById('player-atk');
 const playerDefElement = document.getElementById('player-def');
 const playerAtkSpdElement = document.getElementById('player-atkspd');
 const playerVampElement = document.getElementById('player-vamp');
 const playerCrateElement = document.getElementById('player-crate');
 const playerCdmgElement = document.getElementById('player-cdmg');
-
-const exp = document.querySelector('#exp');
-
-// Gain exp
-// exp.addEventListener("click", function () {
-//     playerExpGain();
-// });
