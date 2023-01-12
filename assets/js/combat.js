39bfea9fecd3d76aa5cec35d6b02a7af73e84f3a
redpangilinan <redpangilinan715@gmail.com>
2023-01-12 18:37:34 +0800

    Added bgm/sfx, handler for combat, and soft reset feature when player dies

--- assets/js/combat.js
+++ assets/js/combat.js
@@ -1,22 +1,79 @@
 const combatPanel = document.querySelector("#combatPanel")
-let battleEnd = false;
+let enemyDead = false;
+let playerDead = false;
 
 // ========== Validation ==========
 const hpValidation = () => {
     // Prioritizes player death before the enemy
     if (player.stats.hp < 1) {
         player.stats.hp = 0;
+        playerDead = true;
         addCombatLog(`You died!`);
+        document.querySelector("#battleButton").addEventListener("click", function () {
+            // Reset all the necessary stats and return to menu
+            let dimDungeon = document.querySelector('#dungeon-main');
+            dimDungeon.style.filter = "brightness(100%)";
+            dimDungeon.style.display = "none";
+            runLoad("title-screen", "flex");
+
+            clearInterval(dungeonTimer);
+            player.stats.hp = player.stats.hpMax;
+            player.lvl = 1;
+            player.exp = {
+                expCurr: 0,
+                expMax: 100,
+                expCurrLvl: 0,
+                expMaxLvl: 100
+            };
+            dungeon.progress.floor = 1;
+            dungeon.progress.room = 1;
+            dungeon.status = {
+                exploring: false,
+                paused: true,
+                event: false,
+            };
+            dungeon.backlog.length = 0;
+            dungeon.runtime = 0;
+            combatBacklog.length = 0;
+            combatPanel.style.display = "none";
+            playerDead = false;
+        });
         endCombat();
     } else if (enemy.stats.hp < 1) {
+        // Gives out all the reward and show the claim button
         enemy.stats.hp = 0;
+        enemyDead = true;
         addCombatLog(`${enemy.name} died! (${new Date(combatSeconds * 1000).toISOString().substring(14, 19)})`);
+        addCombatLog(`You earned ${enemy.rewards.exp} exp.`)
+        playerExpGain();
+        addCombatLog(`${enemy.name} dropped <i class="fas fa-coins" style="color: #FFD700;"></i>${enemy.rewards.gold} gold.`)
+        player.gold += enemy.rewards.gold;
+        playerLoadStats();
+        if (enemy.rewards.drop) {
+            let itemDrop = createEquipment();
+            addCombatLog(`${enemy.name} dropped ${itemDrop}.`)
+        }
+        document.querySelector("#battleButton").addEventListener("click", function () {
+            // Clear combat backlog and transition to dungeon exploration
+            let dimDungeon = document.querySelector('#dungeon-main');
+            dimDungeon.style.filter = "brightness(100%)";
+            bgmDungeon.play();
+
+            player.stats.hp = player.stats.hpMax;
+            player.stats.hpPercent = ((player.stats.hp / player.stats.hpMax) * 100);
+            dungeon.status.event = false;
+            combatPanel.style.display = "none";
+            enemyDead = false;
+            combatBacklog.length = 0;
+        });
         endCombat();
     }
 };
 
 // ========== Attack Functions ==========
 const playerAttack = () => {
+    sfxAttack.play();
+
     // Calculates the damage and attacks the enemy
     let damage = player.stats.atk * (player.stats.atk / (player.stats.atk + enemy.stats.def));
     let lifesteal = player.stats.atk * (player.stats.vamp / 100);
@@ -43,6 +100,8 @@ const playerAttack = () => {
 };
 
 const enemyAttack = () => {
+    sfxAttack.play();
+
     // Calculates the damage and attacks the player
     let damage = enemy.stats.atk * (enemy.stats.atk / (enemy.stats.atk + player.stats.def));
     let lifesteal = enemy.stats.atk * (enemy.stats.vamp / 100);
@@ -88,12 +147,17 @@ const updateCombatLog = () => {
         combatLogBox.appendChild(logElement);
     }
 
-    if (battleEnd) {
+    if (enemyDead) {
         let button = document.createElement("div");
-        button.innerHTML = `
-        <div class="decision-panel">
-            <button id="claimReward">Claim</button>
-        </div>`;
+        button.className = "decision-panel";
+        button.innerHTML = `<button id="battleButton">Claim</button>`;
+        combatLogBox.appendChild(button);
+    }
+
+    if (playerDead) {
+        let button = document.createElement("div");
+        button.className = "decision-panel";
+        button.innerHTML = `<button id="battleButton">Back to Menu</button>`;
         combatLogBox.appendChild(button);
     }
 
@@ -104,6 +168,10 @@ const updateCombatLog = () => {
 let combatSeconds = 0;
 
 const startCombat = () => {
+    bgmDungeon.pause();
+    sfxEncounter.play();
+    bgmBattleMain.play();
+
     // Starts the timer for player and enemy attacks along with combat timer
     let dimDungeon = document.querySelector('#dungeon-main');
     dimDungeon.style.filter = "brightness(50%)";
@@ -121,28 +189,8 @@ const startCombat = () => {
 };
 
 const endCombat = () => {
-    // Gives out all the reward and show the claim button
-    addCombatLog(`You earned ${enemy.rewards.exp} exp.`)
-    playerExpGain();
-    addCombatLog(`${enemy.name} dropped <i class="fas fa-coins" style="color: #FFD700;"></i>${enemy.rewards.gold} gold.`)
-    player.gold += enemy.rewards.gold;
-    playerLoadStats();
-    if (enemy.rewards.drop) {
-        let itemDrop = createEquipment();
-        addCombatLog(`${enemy.name} dropped ${itemDrop}.`)
-    }
-    battleEnd = true;
-    updateCombatLog();
-    document.querySelector("#claimReward").addEventListener("click", function () {
-        let dimDungeon = document.querySelector('#dungeon-main');
-        dimDungeon.style.filter = "brightness(100%)";
-        player.stats.hp = player.stats.hpMax;
-        player.stats.hpPercent = ((player.stats.hp / player.stats.hpMax) * 100);
-        dungeon.status.event = false;
-        combatPanel.style.display = "none";
-        battleEnd = false;
-        combatBacklog.length = 0
-    });
+    bgmBattleMain.stop();
+    sfxCombatEnd.play();
 
     // Stops every timer in combat
     clearInterval(playerTimer);
@@ -161,14 +209,14 @@ const showCombatInfo = () => {
         <div class="battle-info-panel center" id="enemyPanel">
             <p>${enemy.name} Lv.${enemy.lvl}</p>
             <div class="battle-bar empty-bar hp bb-hp">
-                <div class="battle-bar current bb-hp" id="enemy-hp-battle">${nFormatter(enemy.stats.hp)}/${nFormatter(enemy.stats.hpMax)}<br>(${enemy.stats.hpPercent}%)</div>
+                <div class="battle-bar current bb-hp" id="enemy-hp-battle">&nbsp${nFormatter(enemy.stats.hp)}/${nFormatter(enemy.stats.hpMax)}<br>(${enemy.stats.hpPercent}%)</div>
             </div>
             <img src="./assets/sprites/${enemy.image.name}${enemy.image.type}" alt="${enemy.name}" width="${enemy.image.size}">
         </div>
         <div class="battle-info-panel primary-panel">
             <p id="player-combat-info"></p>
             <div class="battle-bar empty-bar bb-hp">
-                <div class="battle-bar current bb-hp" id="player-hp-battle">${nFormatter(player.stats.hp)}/${nFormatter(player.stats.hpMax)}(${player.stats.hpPercent}%)</div>
+                <div class="battle-bar current bb-hp" id="player-hp-battle">&nbsp${nFormatter(player.stats.hp)}/${nFormatter(player.stats.hpMax)}(${player.stats.hpPercent}%)</div>
             </div>
             <div class="battle-bar empty-bar bb-xb">
                 <div class="battle-bar current bb-xb" id="player-exp-bar">exp</div>
