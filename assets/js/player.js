e616e8bd053be5a8aa1c13da4e5c27905506c5a5
redpangilinan <redpangilinan715@gmail.com>
2023-01-12 18:38:56 +0800

    Moved equipment related functions from player to equipment

--- assets/js/player.js
+++ assets/js/player.js
@@ -32,153 +32,6 @@ const playerLevelUp = () => {
     addCombatLog(`You leveled up! (Lv.${player.lvl - 1} > Lv.${player.lvl})`)
 };
 
-const showItemInfo = (item, icon, type, i) => {
-    dungeon.status.exploring = false;
-    let itemInfo = document.querySelector("#equipmentInfo");
-    let target = null;
-    if (type == "Equip") {
-        target = "#inventory";
-    } else if (type == "Unequip") {
-        target = "#dungeon-main";
-    }
-    let dimContainer = document.querySelector(`${target}`);
-    itemInfo.style.display = "flex";
-    dimContainer.style.filter = "brightness(50%)";
-    itemInfo.innerHTML = `
-            <div class="content">
-                <h3 class="${item.rarity}">${icon}${item.rarity} ${item.category}</h3>
-                <ul>
-                ${item.stats.map(stat => {
-        return `<li>${Object.keys(stat)[0]}: ${stat[Object.keys(stat)[0]]}</li>`;
-    }).join('')}
-                </ul>
-                <div class="button-container">
-                    <button id="un-equip">${type}</button>
-                    <button id="discard">Discard</button>
-                    <button id="close-item-info">Close</button>
-                </div>
-            </div>`;
-
-    // Equip button for the item
-    let unEquip = document.querySelector("#un-equip");
-    unEquip.addEventListener('click', function () {
-        if (type == "Equip") {
-            // Remove the item from the inventory and add it to the equipment
-            itemInfo.style.display = "none";
-            dimContainer.style.filter = "brightness(100%)";
-            if (player.equipped.length >= 6) {
-                alert("You are fully equipped.");
-            } else {
-                player.inventory.equipment.splice(i, 1);
-                player.equipped.push(item);
-            }
-            playerLoadStats();
-            saveData();
-            continueExploring();
-        } else if (type == "Unequip") {
-            // Remove the item from the equipment and add it to the inventory
-            itemInfo.style.display = "none";
-            dimContainer.style.filter = "brightness(100%)";
-            player.equipped.splice(i, 1);
-            player.inventory.equipment.push(JSON.stringify(item));
-            playerLoadStats();
-            saveData();
-            continueExploring();
-        }
-    });
-
-    // Close item info
-    let close = document.querySelector("#close-item-info");
-    close.addEventListener('click', function () {
-        itemInfo.style.display = "none";
-        dimContainer.style.filter = "brightness(100%)";
-        continueExploring();
-    });
-}
-
-// Show inventory
-const showInventory = () => {
-    // Clear the inventory container
-    let playerInventoryList = document.getElementById("playerInventory");
-    playerInventoryList.innerHTML = "";
-
-    if (player.inventory.equipment.length == 0) {
-        playerInventoryList.innerHTML = "There are no items available.";
-    }
-
-    for (let i = 0; i < player.inventory.equipment.length; i++) {
-        const item = JSON.parse(player.inventory.equipment[i]);
-
-        // Create an element to display the item's name
-        let itemDiv = document.createElement('div');
-        let icon = equipmentIcon(item.category);
-        itemDiv.className = "items";
-        itemDiv.innerHTML = `<p class="${item.rarity}">${icon}${item.rarity} ${item.category}</p>`;
-        itemDiv.addEventListener('click', function () {
-            let type = "Equip";
-            showItemInfo(item, icon, type, i);
-        });
-
-        // Add the itemDiv to the inventory container
-        playerInventoryList.appendChild(itemDiv);
-    }
-};
-
-// Show equipment
-const showEquipment = () => {
-    // Clear the inventory container
-    let playerEquipmentList = document.getElementById("playerEquipment");
-    playerEquipmentList.innerHTML = "";
-
-    // Show a message if a player has no equipment
-    if (player.equipped.length == 0) {
-        playerEquipmentList.innerHTML = "Nothing equipped.";
-    }
-
-    for (let i = 0; i < player.equipped.length; i++) {
-        const item = player.equipped[i];
-
-        // Create an element to display the item's name
-        let equipDiv = document.createElement('div');
-        let icon = equipmentIcon(item.category);
-        equipDiv.className = "items";
-        equipDiv.innerHTML = `<p class="${item.rarity}">${icon}${item.rarity} ${item.category}</p>`;
-        equipDiv.addEventListener('click', function () {
-            let type = "Unequip";
-            showItemInfo(item, icon, type, i);
-        });
-
-        // Add the equipDiv to the inventory container
-        playerEquipmentList.appendChild(equipDiv);
-    }
-};
-
-// Apply the equipment stats to the player
-const applyEquipmentStats = () => {
-    // Reset the equipment stats
-    player.equippedStats = {
-        hp: 0,
-        atk: 0,
-        def: 0,
-        atkSpd: 0,
-        vamp: 0,
-        critRate: 0,
-        critDmg: 0
-    };
-
-    for (let i = 0; i < player.equipped.length; i++) {
-        const item = player.equipped[i];
-
-        // Iterate through the stats array and update the player stats
-        item.stats.forEach(stat => {
-            for (const key in stat) {
-                player.equippedStats[key] += stat[key];
-            }
-        });
-    }
-    calculateStats();
-}
-
 // Refresh the player stats
 const playerLoadStats = () => {
     showEquipment();
@@ -202,7 +55,7 @@ const playerLoadStats = () => {
 
     // Player Stats
     playerHpElement.innerHTML = `${nFormatter(player.stats.hp)}/${nFormatter(player.stats.hpMax)} (${player.stats.hpPercent}%)`;
-    playerCombatHpElement.innerHTML = `${nFormatter(player.stats.hp)}/${nFormatter(player.stats.hpMax)}(${player.stats.hpPercent}%)`;
+    playerCombatHpElement.innerHTML = `&nbsp${nFormatter(player.stats.hp)}/${nFormatter(player.stats.hpMax)}(${player.stats.hpPercent}%)`;
     playerAtkElement.innerHTML = nFormatter(player.stats.atk);
     playerDefElement.innerHTML = nFormatter(player.stats.def);
     playerAtkSpdElement.innerHTML = (player.stats.atkSpd).toFixed(1).replace(rx, "$1");
@@ -213,6 +66,9 @@ const playerLoadStats = () => {
 
 const openInventory = () => {
     dungeon.status.exploring = false;
+    let scroll = document.querySelector('#playerInventory')
+    scroll.scrollBottom - scroll.clientHeight;
+
     inventoryOpen = true;
     let openInv = document.querySelector('#inventory');
     let dimDungeon = document.querySelector('#dungeon-main');
