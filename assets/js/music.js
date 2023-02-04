013bf305c0c8513f3b4f26d324adc872e963bd4f
redpangilinan <redpangilinan715@gmail.com>
2023-02-04 16:38:24 +0800

    v1.1.3: Update music src

--- assets/js/music.js
+++ assets/js/music.js
@@ -37,102 +37,102 @@ let sfxBuff;
 const setVolume = () => {
     // ===== BGM =====
     bgmDungeon = new Howl({
-        src: ['/assets/bgm/dungeon.wav'],
+        src: ['./assets/bgm/dungeon.wav'],
         volume: volume.bgm * volume.master,
         loop: true
     });
 
     bgmBattleMain = new Howl({
-        src: ['/assets/bgm/battle_main.wav'],
+        src: ['./assets/bgm/battle_main.wav'],
         volume: volume.bgm * volume.master,
         loop: true
     });
 
     bgmBattleBoss = new Howl({
-        src: ['/assets/bgm/battle_boss.mp3'],
+        src: ['./assets/bgm/battle_boss.mp3'],
         volume: volume.bgm * volume.master,
         loop: true
     });
 
     bgmBattleGuardian = new Howl({
-        src: ['/assets/bgm/battle_guardian.wav'],
+        src: ['./assets/bgm/battle_guardian.wav'],
         volume: volume.bgm * volume.master,
         loop: true
     });
 
     // ===== SFX =====
     sfxEncounter = new Howl({
-        src: ['/assets/sfx/encounter.wav'],
+        src: ['./assets/sfx/encounter.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxCombatEnd = new Howl({
-        src: ['/assets/sfx/combat_end.wav'],
+        src: ['./assets/sfx/combat_end.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxAttack = new Howl({
-        src: ['/assets/sfx/attack.wav'],
+        src: ['./assets/sfx/attack.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxLvlUp = new Howl({
-        src: ['/assets/sfx/level_up.wav'],
+        src: ['./assets/sfx/level_up.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxConfirm = new Howl({
-        src: ['/assets/sfx/confirm.wav'],
+        src: ['./assets/sfx/confirm.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxDecline = new Howl({
-        src: ['/assets/sfx/decline.wav'],
+        src: ['./assets/sfx/decline.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxDeny = new Howl({
-        src: ['/assets/sfx/denied.wav'],
+        src: ['./assets/sfx/denied.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxEquip = new Howl({
-        src: ['/assets/sfx/equip.wav'],
+        src: ['./assets/sfx/equip.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxUnequip = new Howl({
-        src: ['/assets/sfx/unequip.wav'],
+        src: ['./assets/sfx/unequip.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxOpen = new Howl({
-        src: ['/assets/sfx/hover.wav'],
+        src: ['./assets/sfx/hover.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxPause = new Howl({
-        src: ['/assets/sfx/pause.wav'],
+        src: ['./assets/sfx/pause.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxUnpause = new Howl({
-        src: ['/assets/sfx/unpause.wav'],
+        src: ['./assets/sfx/unpause.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxSell = new Howl({
-        src: ['/assets/sfx/sell.wav'],
+        src: ['./assets/sfx/sell.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxItem = new Howl({
-        src: ['/assets/sfx/item_use.wav'],
+        src: ['./assets/sfx/item_use.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxBuff = new Howl({
-        src: ['/assets/sfx/buff.wav'],
+        src: ['./assets/sfx/buff.wav'],
         volume: volume.sfx * volume.master
     });
 }
